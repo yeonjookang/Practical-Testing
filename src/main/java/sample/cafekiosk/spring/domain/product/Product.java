4ac3ec98a00d6cf3ec81c853931b9fcbb2073a33
kang yeon joo <rkdduswndi@naver.com>
2024-05-15 17:25:19 +0900

    feat: lesson 5-4

--- src/main/java/sample/cafekiosk/spring/domain/product/Product.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.domain.product;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
@@ -20,4 +21,13 @@ public class Product extends BaseEntity {
     private ProductSellingType sellingType;
     private String name;
     private int price;
+
+    @Builder
+    private Product(String productNumber, ProductType type, ProductSellingType sellingType, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingType = sellingType;
+        this.name = name;
+        this.price = price;
+    }
 }
